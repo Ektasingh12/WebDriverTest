b6615ce8cc61b5eb2205c9cdd9fd927e74e8d77c
ppanagod <ppanagod@waikato.ac.nz>
2018-07-25 09:02:05 +1200

    first commit

---
+++ src/test/java/example/NewTest.java
@@ -0,0 +1,53 @@
+package example;		
+
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.testng.Assert;		
+import org.testng.annotations.Test;
+
+import com.google.errorprone.annotations.Var;
+
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;		
+public class NewTest {		
+	public String baseURL ="http://demo.guru99.com/test/guru99home/";
+
+	public WebDriver driver;	
+		@Test				
+		public void testEasy() {
+			
+			
+			System.out.println("launching firfox");
+			  System.setProperty("webdriver.gecko.driver", "C://Users//sam//Music//ekta//geckodriver.exe");
+			  driver= new FirefoxDriver();
+			  driver.get(baseURL);
+			  String expectedTitle ="Demo Guru99 Page";
+			  String actualTitle = driver.getTitle();
+			  Assert.assertEquals(actualTitle, expectedTitle);
+			  
+			  
+				
+		}	
+		
+		
+		
+
+		//@BeforeTest
+		//public void beforeTest() {	
+		//    driver = new FirefoxDriver();  
+		//}	
+		
+		@AfterTest
+	public void afterTest() {
+		driver.quit();			
+		}		
+}
+
+
+
+
+
+
+
